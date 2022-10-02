a5aacd0509eaa8a7bb550ea46fadec9c07f7b2a5
21A91A05C0 <97424374+21A91A05C0@users.noreply.github.com>
2022-10-02 21:54:34 +0530

    import java.util.*; class n {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         double a=sc.nextDouble();         if(a%2==0)         {             System.out.println("Even");         }         else         {             System.out.println("Odd");         }     } }

---
+++ Check_if_number_is_even_or_odd.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class n
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        double a=sc.nextDouble();
+        if(a%2==0)
+        {
+            System.out.println("Even");
+        }
+        else
+        {
+            System.out.println("Odd");
+        }
+    }
+}
