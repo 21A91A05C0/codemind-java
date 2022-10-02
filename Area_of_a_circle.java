cb555e02298614b48275a109876b48a72f1ac3b1
21A91A05C0 <97424374+21A91A05C0@users.noreply.github.com>
2022-10-02 21:31:19 +0530

    import java.util.*; class n {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         double a=sc.nextDouble();         System.out.format("%.2f",a*a*3.14);     } }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class n
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        double a=sc.nextDouble();
+        System.out.format("%.2f",a*a*3.14);
+    }
+}
