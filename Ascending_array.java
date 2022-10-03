397e5386b4257f95ea72b2ebad386d6ec9d70de8
21A91A05C0 <97424374+21A91A05C0@users.noreply.github.com>
2022-10-03 21:19:32 +0530

    import java.util.*; class sowji {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int[] arr=new int[n];         int c=0;         for(int i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         for(int i=1;i<n;i++)         {             if(arr[i-1]<arr[i])             {                 c=c+1;             }         }         if(c==n-1)         {             System.out.println("yes");         }         else         {             System.out.println("no");         }     } }

---
+++ Ascending_array.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class sowji
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        int c=0;
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        for(int i=1;i<n;i++)
+        {
+            if(arr[i-1]<arr[i])
+            {
+                c=c+1;
+            }
+        }
+        if(c==n-1)
+        {
+            System.out.println("yes");
+        }
+        else
+        {
+            System.out.println("no");
+        }
+    }
+}
