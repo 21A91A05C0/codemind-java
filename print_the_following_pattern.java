61249e75548b6ea96ffe3f86129dd55548d8e990
21A91A05C0 <97424374+21A91A05C0@users.noreply.github.com>
2023-02-04 21:17:26 +0530

    import java.util.*; class print{     public static void main(String args[])     {         int n;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         for(int i=1;i<=n;i++)         {             for(int j=1;j<=n-i+1;j++)             {                 System.out.print(j);             }             System.out.println();         }     } }

---
+++ print_the_following_pattern.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class print{
+    public static void main(String args[])
+    {
+        int n;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=n-i+1;j++)
+            {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
