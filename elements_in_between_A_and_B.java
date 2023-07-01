01a89eb6cd4443fd213a566daad8abb34b599891
21A91A05C0 <97424374+21A91A05C0@users.noreply.github.com>
2023-07-01 22:52:39 +0530

    import java.util.*; public class l {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int a[]=new int[n];
            for(int i=0;i<n;i++)
            {
                a[i]=sc.nextInt();
            }
            int z=sc.nextInt();
            int b=sc.nextInt();
            int d=0;
            for(int i=0;i<n;i++)
            {
                if(a[i]>=z && a[i]<=b)
                {
                    System.out.format("%d ",a[i]);
                    d=d+1;
                }
            }
            if(d==0)
            {
                System.out.println(-1);
            }
            
            
        }
    }

---
+++ elements_in_between_A_and_B.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class l
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        int z=sc.nextInt();
+        int b=sc.nextInt();
+        int d=0;
+        for(int i=0;i<n;i++)
+        {
+            if(a[i]>=z && a[i]<=b)
+            {
+                System.out.format("%d ",a[i]);
+                d=d+1;
+            }
+        }
+        if(d==0)
+        {
+            System.out.println(-1);
+        }
+        
+        
+    }
+}
